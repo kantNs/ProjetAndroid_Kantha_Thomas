282bbc6b797b5ca57fb1e1eea74c4be7db74b816
kantNS <thebosssdu69@gmail.com>
2018-04-10 18:52:13 +0200

    ajout menu avec boutons quit, ajout vue landscape et ajout traduction anglaise.

--- app/src/main/java/com/example/kentha/projetandroid_kantha_thomas/MainActivity.java
+++ app/src/main/java/com/example/kentha/projetandroid_kantha_thomas/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -37,6 +39,20 @@ public class MainActivity extends AppCompatActivity {
         zoneEdition = (EditText) findViewById(R.id.editText);
         String api_url = getResources().getString(R.string.api_url) + zoneEdition.getText().toString()+"&apikey=ab007825";
         RecupJson(api_url);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.quit:
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void Traitement(){
